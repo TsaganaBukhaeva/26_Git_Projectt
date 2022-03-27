a632acbc946fda7020b79f22d27c2c90b91d3e1e
Galsan <Angira8080@gmail.com>
2022-03-27 13:14:07 -0400

    First class updated

--- src/day0/First.java
+++ src/day0/First.java
@@ -4,6 +4,7 @@ public class First {
     public static void main(String[] args) {
         System.out.println("java");
         System.out.println("working");
+        //added new  codes
     }
 
     //extra line 1
